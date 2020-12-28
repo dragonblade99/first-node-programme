bf8e7f0e89a1a6177a45dbd1bde46451f1761a3b
Vishal Kumar Rathore <dragonblade2799@gmail.com>
2020-12-28 14:00:10 +0530

    os in node

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-import log from "./logger.js";
-log("message");
+// import log from "./logger.js";
+// log("message");
+
+// const os = require("os");
+
+import os from "os";
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+console.log(`Total memory:${totalMemory}`);
+console.log(`Free memory:${freeMemory}`);
