1c5380a57811b969f7051a407312471ded3e2fa9
Vishal Kumar Rathore <dragonblade2799@gmail.com>
2020-12-28 14:02:29 +0530

    file system module

--- app.js
+++ app.js
@@ -3,10 +3,19 @@
 
 // const os = require("os");
 
-import os from "os";
+// import os from "os";
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
+// var totalMemory = os.totalmem();
+// var freeMemory = os.freemem();
 
-console.log(`Total memory:${totalMemory}`);
-console.log(`Free memory:${freeMemory}`);
+// console.log(`Total memory:${totalMemory}`);
+// console.log(`Free memory:${freeMemory}`);
+
+import fs from "fs";
+// const files = fs.readdirSync("./");
+// console.log(files);
+
+fs.readdir("./", function (err, files) {
+  if (err) console.log("error", err);
+  else console.log("result", files);
+});
