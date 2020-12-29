d6f9e8ca9d729e3bb2293cd39cafb32edcfe7912
Vishal Kumar Rathore <dragonblade2799@gmail.com>
2020-12-29 13:37:03 +0530

    http advance

--- app.js
+++ app.js
@@ -44,10 +44,15 @@
 
 import http from "http";
 
-const server = http.createServer();
-
-server.on("connection", (socket) => {
-  console.log("new conection...");
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.write("hello world");
+    res.end();
+  }
+  if (req.url === "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
 });
 
 server.listen(3000);
